f2354d8c2914b4f76a3f326313a19e3be1928c9f
Muhammad Saad Kamal <Msaadkamal1999@gmail.com>
2023-11-02 00:15:49 +0500

    Add Bootstrap file in Github

---
+++ script.js
@@ -0,0 +1,28 @@
+// CounterAnimation
+let nCount=function(selector){
+    $(selector).each(function(){
+        $(this).animate({
+            Counter:$(this).text()
+
+        },{
+            duration:4000,
+            easing:"swing",
+            step:function (value) {
+                $(this).text(Math.ceil(value))
+            }
+                
+            }
+        );
+     })
+};
+let a=0;
+$(window).scroll(function(){
+    let oTop=$(".numbers").offset().top-Window.innerHeight;
+    if(a==0 && $(window).scrollTop()>= oTop){
+        a++;
+        nCount(".rect>h1")
+    }
+
+
+    
+});
